2d0664ecc90476336f1fe5061ba3c3270a9fd902
Sahrul Yudha Farhrezi <yudhafahrezi30@gmail.com>
2024-07-15 08:09:09 +0700

    Add requirement dotenv

--- get_token.py
+++ get_token.py
@@ -1,10 +1,10 @@
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 import base64
 from requests import post, get
 import json
 
-load_dotenv()
+# load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
